7356410d26ce076c89a8fbf1111f2e47bd4a06f0
manuela-crr <manuela.carriero1@gmail.com>
2022-04-03 16:05:17 +0200

    added a toy exercize

---
+++ toy.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  3 16:01:17 2022
+
+@author: asus
+"""
+
+import numpy as np
+
+n=10
+numbers = np.arange(n+1) 
+print(numbers)
+
+for i in numbers: 
+    print("I am happy")
