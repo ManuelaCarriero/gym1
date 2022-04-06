bbb2304291f61b0935002d627171587d1fecd902
manuela-crr <manuela.carriero1@gmail.com>
2022-04-06 09:53:18 +0200

    added empty class

---
+++ oop.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  6 09:45:40 2022
+
+@author: asus
+"""
+import pdir as dir
+
+class Empty():
+    """ This is an empty class. 
+    It does nothing."""
+    
+#Let us define our instance
+
+empty_instance = Empty()
+
+help(empty_instance)
+
+dir(empty_instance).public
