5427aa9e4090321c9d3b37a849f82ce2f1a634a7
manuela-crr <manuela.carriero1@gmail.com>
2022-04-07 17:02:57 +0200

    added __init__ method examples

--- oop.py
+++ oop.py
@@ -39,3 +39,35 @@ instance_of_Something = Something()
 instance_of_Something.multiply(b=3) #bound
 
 Something.multiply(instance_of_Something,b=3) #unbound
+
+#__init__ method
+
+class Something:
+    def __init__(self,a):
+        self.a = a
+
+instance_mary = Something(a=3)
+instance_mary.a
+
+"""
+OOP Exercise 1: Create a Class with instance attributes
+Write a Python program to create a Vehicle class with max_speed 
+and mileage instance attributes.
+"""
+class Vehicle:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+        
+# This is an instance of Vehicle !
+ModelX = Vehicle(max_speed = 200, mileage = 10) 
+
+Vehicle.max_speed 
+#AttributeError: type object 'Vehicle' has no attribute 'max_speed'
+
+ModelX.max_speed 
+#Se non metti il self ti dà lo stesso AttributeError precedente
+
+"""Thus __init__ sets the attributes of the instances 
+and not of the classes"""
+
